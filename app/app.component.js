ab73b70b28f6d2f9067a42f82a0910f9fb5d7eef
lgc <lapusneanugabi@yahoo.com>
2016-11-06 16:05:42 +0200

    switch to class

--- app/app.component.js
+++ app/app.component.js
@@ -14,7 +14,7 @@ var AppComponent = (function () {
     }
     AppComponent = __decorate([
         core_1.Component({
-            selector: 'my-app',
+            selector: '.my-app',
             template: "1+2 =  (( '{{1+ 2}}' )) | 1+2 = ((1+2))",
             interpolation: ['((', '))']
         }), 
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
-  selector: 'my-app',
+  selector: '.my-app',
   template: `1+2 =  (( '{{1+ 2}}' )) | 1+2 = ((1+2))`,
   interpolation: ['((', '))']
 })
